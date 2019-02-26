7b9ea7ba772a6dfac2845f10ec06286cb7c047f2
aallenchen2018 <42566084+aallenchen2018@users.noreply.github.com>
2019-02-26 21:02:32 +0800

    2.26

--- socketclient.py
+++ socketclient.py
@@ -1,8 +1,11 @@
 import socket
-ip_port=('127.0.0.1',9988)
-sk=socket.socket()
-sk.connect(ip_port)
-sk.sendall(('im client!').encode())
-ser_reply=sk.recv(1024)
-print(ser_reply)
-sk.close()
+ip_port=('192.168.4.186',9999)
+while 1:
+    sk=socket.socket()
+    sk.connect(ip_port)
+    # while 1:
+    sr=input('write:   ')
+    sk.sendall(('%s' %sr).encode())
+    ser_reply=sk.recv(1024)
+    print(ser_reply)
+    sk.close()
--- 多进程.py
+++ 多进程.py
@@ -14,3 +14,8 @@ if __name__=='__main__':
     pl.join()
 
 ###
+
+
+def test(x,y):
+    print(x+y)
+    
