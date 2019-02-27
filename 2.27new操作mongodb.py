6372d239094b21d1009f25cf89c11bfe77e3c73e
aallenchen2018 <42566084+aallenchen2018@users.noreply.github.com>
2019-02-27 18:42:55 +0800

    2.27晚上

--- 2.27new操作mongodb.py
+++ 2.27new操作mongodb.py
@@ -2,33 +2,29 @@
 import pymongo
 conn=pymongo.MongoClient(host='127.0.0.1',port=27017)
 db=conn.toto  #名字叫toto的数据库, 下面建立一个叫做jianli的表格
-# db.jianli.insert([{'name':'wuyi','tel':566787,'exp':3},{'name':'kangshao','tel':2256677,'exp':0},{'name':'dabiaoge','tel':44997990,'exp':3}])
-
 
 while 1:
-	
-
 	chose=input('1-查询;2-增加:3-删减;4-更新;5-预览表格')
 	#查询
 	if chose =='1':
-		chosefind=input('name?')
+		chosefind=input('输入名字')
 		namefind=db.jianli.find({'name':chosefind},{'_id':0})
 		for i in namefind:
 			print(i)
 	#增加(如果表中有此名字,不会添加;否则添加)
 	elif chose=='2':
-		add=input('name?')
+		add=input('输入名字')
 		check=db.jianli.find({'name':add})
 		if check.count()!=0:
 			print('此名字已有')
 		else:
-			add_tel=int(input('tel'))
-			add_exp=int(input('exp'))
+			add_tel=int(input('输入电话'))
+			add_exp=int(input('输入经历'))
 			db.jianli.insert({'name':add,'tel':add_tel,'exp':add_exp})		
 	#删减(如果表中无此名字,不会删除;否则删除)
 	elif chose=='3':
 		
-		del2=input('name')
+		del2=input('输入名字')
 		check=db.jianli.find({'name':del2})
 		if check.count()!=0:
 			db.jianli.remove({'name':del2})
@@ -37,9 +33,9 @@ while 1:
 
 	#修改
 	elif chose=='4':
-		update_name=input('update name?')
-		update_tel=int(input('update tel?'))
-		update_exp=int(input('update exp?'))	
+		update_name=input('输入修改的名字')
+		update_tel=int(input('输入修改的电话'))
+		update_exp=int(input('输入修改的经历'))	
 		db.jianli.update({'name':update_name},{'$set':{'tel':update_tel,'exp':update_exp}})
 	#阅览
 	elif chose=='5':
